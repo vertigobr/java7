a802e50bc986ce12ec1dc3f2ca0f253a8b3dc7bc
Rubens <rubens@vertigo.com.br>
2016-06-02 21:26:53 +0000

    Update local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -2,4 +2,4 @@
 LOCALJDK="http://192.168.1.2/files/oracle/jdk-7u79-linux-x64.rpm"
 docker build \
     --build-arg "LOCALJDK=$LOCALJDK" \
-    -t vertigo/java7 .
+    -t rubens/Liferay62cesMysqlSolr .
